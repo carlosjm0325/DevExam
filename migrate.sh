d994605c5b8097d0d4c95437335c6ea098c0803c
carlosjm0325 <carlosjm0325@gmail.com>
2021-01-04 11:31:57 -0500

    script updates

--- migrate.sh
+++ migrate.sh
@@ -4,6 +4,9 @@ cd /srv/app
 composer update
 php artisan migrate:fresh --seed --force
 php artisan key:generate
+chmod 777 -R storage
+chmod 777 -R bootstrap/cache
+cp env.example .env
 
 # execute apache
 /usr/sbin/apache2ctl -D FOREGROUND;

d3e98760a3fc6c680dd371dae00469686c971553
carlosjm0325 <carlosjm0325@gmail.com>
2021-01-03 20:00:12 -0500

    updating base to work with docker

--- server/config/database.php
+++ server/config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DATABASE_HOST','localhost'),
+            'host' => env('DATABASE_HOST','db'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'basedb'),
             'username' => env('DB_USERNAME', 'root'),
